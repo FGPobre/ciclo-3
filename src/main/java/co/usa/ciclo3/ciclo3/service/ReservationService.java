a66a365ad54dbd21939f3dc29eb6806f8e2da8c0
FGPobre <fredy.gonzalez.myorreo.usa.edu.co>
2021-10-24 14:01:12 -0500

    formato de fecha corregido Reto 3

--- src/main/java/co/usa/ciclo3/ciclo3/service/ReservationService.java
+++ src/main/java/co/usa/ciclo3/ciclo3/service/ReservationService.java
@@ -32,25 +32,30 @@ public class ReservationService {
     }
     public Reservation save(Reservation lb){
         if(lb.getIdReservation()==null){
-            LocalDate fechaini =LocalDate.parse(lb.getStartDate());
-            LocalDate fechad=LocalDate.parse(lb.getDevolutionDate());
-            LocalTime lt = LocalTime.of( 0 , 0 ) ;
-            OffsetDateTime fechaini_of = OffsetDateTime.of( fechaini , lt , ZoneOffset.UTC ) ;
-            OffsetDateTime fechad_of = OffsetDateTime.of( fechad , lt , ZoneOffset.UTC ) ;
-            DateTimeFormatter f = DateTimeFormatter.ofPattern( "uuuu-MM-dd'T'HH:mm:ss.SSSZ" , Locale.US );
-            lb.setStartDate(fechaini_of.format(f));
-            lb.setDevolutionDate(fechad_of.format(f));
-            return reservationRepository.save(lb);
+        LocalDate fechaini =LocalDate.parse(lb.getStartDate());
+        LocalDate fechad=LocalDate.parse(lb.getDevolutionDate());
+        LocalTime lt = LocalTime.of(0,0) ;
+        OffsetDateTime fechaini_of = OffsetDateTime.of( fechaini , lt , ZoneOffset.UTC ) ;
+        OffsetDateTime fechad_of = OffsetDateTime.of( fechad , lt , ZoneOffset.UTC ) ;
+        DateTimeFormatter f = DateTimeFormatter.ofPattern( "uuuu-MM-dd'T'HH:mm:ss.SSSZ" , Locale.US );
+        String Fecha_fi=fechaini_of.format(f);
+        String Fecha_ff=fechad_of.format(f);
+        String nueva_Fecha_fi=Fecha_fi.substring(0, 26)+":"+Fecha_fi.substring(26);
+        String nueva_Fecha_ff=Fecha_ff.substring(0, 26)+":"+Fecha_fi.substring(26);
+        lb.setStartDate(nueva_Fecha_fi);
+        lb.setDevolutionDate(nueva_Fecha_ff);
+        return reservationRepository.save(lb);
+         
         }
         else{
             Optional<Reservation> lbc=reservationRepository.getReservation(lb.getIdReservation());
             if(!lbc.isPresent()){
                 LocalDate fechaini =LocalDate.parse(lb.getStartDate());
                 LocalDate fechad=LocalDate.parse(lb.getDevolutionDate());
-                LocalTime lt = LocalTime.of( 0 , 0 ) ;
+                LocalTime lt = LocalTime.of(0,0);
                 OffsetDateTime fechaini_of = OffsetDateTime.of( fechaini , lt , ZoneOffset.UTC ) ;
                 OffsetDateTime fechad_of = OffsetDateTime.of( fechad , lt , ZoneOffset.UTC ) ;
-                DateTimeFormatter f = DateTimeFormatter.ofPattern( "uuuu-MM-dd'T'HH:mm:ss.SSSZ" , Locale.US );
+                DateTimeFormatter f = DateTimeFormatter.ofPattern( "uuuu-MM-dd'T'HH:mm:ss.SSSZ" , Locale.ROOT );
                 lb.setStartDate(fechaini_of.format(f));
                 lb.setDevolutionDate(fechad_of.format(f));
                 return reservationRepository.save(lb);
